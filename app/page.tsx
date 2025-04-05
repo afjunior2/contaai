d89af6391608a8ba3e9d3e66669e084105579094
Antonio Fernando da Silva Junior <afs_jr@hotmail.com>
2025-04-05 12:52:40 -0300

    setup front

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+      <main className="flex min-h-screen items-center justify-center bg-green-100">
+        <h1 className="text-3xl font-bold text-green-900">ContaAI 🚀</h1>
+      </main>
+  )
+}
